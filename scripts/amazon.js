0b8cd37c4375b10144300980418b8b0d1e49b166
Stanley Owarieta <owarieta24@outlook.com>
2024-09-11 22:37:20 +0100

    make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -81,6 +81,15 @@ document.querySelectorAll('.js-add-cart')
         quantity: 1
       });
       }
-      console.log(cart)
+
+      let cartQuantity = 0;
+
+      cart.forEach((item) => {
+        cartQuantity += item.quantity;
+      });
+
+
+      document.querySelector('.js-cart-quantity')
+        .innerHTML = cartQuantity;
     });
   });
